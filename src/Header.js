2ccac42d671c36ebd4d8dc1dc795a0470439d834
Manish Kumar <manish15052000@gmail.com>
2021-07-06 00:15:14 +0530

    Improved style

--- src/Header.js
+++ src/Header.js
@@ -21,7 +21,3 @@ export default function Header(props) {
     </header>
   );
 }
-
-// Line 17:13:  The href attribute is required for an anchor to be keyboard accessible. Provide a valid, navigable address as the href value. If you cannot provide an href, but still need the element to resemble a link, use a button and change it with appropriate styles. Learn more: https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md  jsx-a11y/anchor-is-valid
-
-// Search for the keywords to learn more about each warning.
